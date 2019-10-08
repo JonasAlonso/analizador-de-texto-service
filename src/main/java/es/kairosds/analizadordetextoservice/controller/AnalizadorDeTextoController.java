796b0ddc94a77964758ffeec547b7e81420bfc9c
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-08 17:28:48 +0200

    Controlador

---
+++ src/main/java/es/kairosds/analizadordetextoservice/controller/AnalizadorDeTextoController.java
@@ -0,0 +1,10 @@
+package es.kairosds.analizadordetextoservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/analizador")
+public class AnalizadorDeTextoController {
+
+}
