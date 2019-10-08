b579d948f928652202500ff6b6a5a8b0b91631c4
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-08 17:28:31 +0200

    Lector de archivos de palabras prohibidas

---
+++ src/main/java/es/kairosds/analizadordetextoservice/util/LectorDeArchivos.java
@@ -0,0 +1,32 @@
+package es.kairosds.analizadordetextoservice.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class LectorDeArchivos {
+	
+	public static String obtenerListaNegra() {
+		
+
+		StringBuilder sb = new StringBuilder();
+
+		try (BufferedReader br = Files.newBufferedReader(Paths.get("listanegra.txt"))) {
+
+			// read line by line
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+
+		} catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+		}
+
+		return sb.toString();
+
+	}
+
+}
