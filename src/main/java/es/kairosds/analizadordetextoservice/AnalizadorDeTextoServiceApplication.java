3972c5e4603737dedb269a0204e54d6047bacddc
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-08 11:08:19 +0200

    primer commit

---
+++ src/main/java/es/kairosds/analizadordetextoservice/AnalizadorDeTextoServiceApplication.java
@@ -0,0 +1,13 @@
+package es.kairosds.analizadordetextoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizadorDeTextoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalizadorDeTextoServiceApplication.class, args);
+	}
+
+}
