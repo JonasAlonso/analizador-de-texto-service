9b32925e981f53552caedfff64e52085ff54cce7
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-09 16:20:12 +0200

    Llamada al servicio analizador de lenguaje ofensivo

--- src/main/java/es/kairosds/analizadordetextoservice/controller/AnalizadorDeTextoController.java
+++ src/main/java/es/kairosds/analizadordetextoservice/controller/AnalizadorDeTextoController.java
@@ -1,10 +1,33 @@
 package es.kairosds.analizadordetextoservice.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.kairosds.analizadordetextoservice.model.RespuestaAnalisis;
+import es.kairosds.analizadordetextoservice.model.Texto;
+import es.kairosds.analizadordetextoservice.service.AnalizadorService;
 
 @RestController
 @RequestMapping("/analizador")
 public class AnalizadorDeTextoController {
 
+	@Autowired
+	AnalizadorService analizadorService;
+
+	@PostMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<RespuestaAnalisis> analizarComentario(@RequestBody Texto texto){
+		
+		RespuestaAnalisis analisis = RespuestaAnalisis.builder().lenguajeOfensivo(analizadorService.analizarTexto(texto.getContenido())).build();
+		
+		return ResponseEntity.ok().body(analisis);	
+	}
 }
